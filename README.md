d03f7a07a39ee90cb4acfb2a3cea3ba68ec1caa4
Umberto <umbertofazio92@gmail.com>
2019-11-05 15:11:24 +0100

    readme

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 This simple Web App displays a random generated graph with 10 nodes.
 
-If you click on a node the property of that node are shown on the right.
+If you click on a node the properties of that node are shown on the right.
 
 The properties are position (x and y) and the outgoing edges.
 
-You can modify the node's propertis. When you click "Update" the graph will be updated
+You can modify the node's properties. When you click "Update" the graph will be updated.
 
 ![screenshot](screen_graph.png)
--- app.py
+++ app.py
@@ -60,11 +60,11 @@ def graph_traces(G):
 introductive_text = '''
 This simple Web App displays a random generated graph with 10 nodes.
 
-If you click on a node the property of that node are shown on the right.
+If you click on a node the properties of that node are shown on the right.
 
 The properties are position (x and y) and the outgoing edges.
 
-You can modify the node's propertis. When you click "Update" the graph will be updated'''
+You can modify the node's properties. When you click "Update" the graph will be updated.'''
 app.layout = html.Div([html.H1(children='Graph Task'),
     dcc.Markdown(children = introductive_text),
 
